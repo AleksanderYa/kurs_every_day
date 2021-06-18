f23a61f8042060db8732aa3694e9a575aed6c8e8
AleksanderYa <250000ye@gmail.com>
2021-06-18 16:38:52 +0300

    add get_kurs.py

--- T/T/settings.py
+++ T/T/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'testy',
+    'currency.apps.CurrencyConfig',
+    'testy.apps.TestyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
