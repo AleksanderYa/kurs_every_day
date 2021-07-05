69e6686ec26d9aecc88f44c7a7345d360e5c4789
AleksanderYa <250000ye@gmail.com>
2021-07-05 17:29:07 +0300

    Add TODO.end work.add get_kurs

--- T/currency/views.py
+++ T/currency/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from currency.models import Currency as c
 from currency.get_kurs import get_privat_currency
 from currency.telega2 import telegram_bot_sendtext
@@ -21,9 +21,6 @@ def button_to_base(request):
     }
     return render(request, 'currency/button_to_base.html', dictt)
 
-def get_currency_to_base(request):
-    return redirect(request, 'currency/button_to_base.html')
-
 def button(request):
     return render(request, 'currency/button.html')
 
@@ -37,11 +34,23 @@ def send_mess_telega(request):
 def mirror(request):
     base_dict = dic.copy()
     base_dict['title'] = 'Mirror'
+    cobject = c.objects.all()[::-1]
     base_dict['all_kurs'] = c.objects.all()
     message = f'USD{nbu.nbu_usd}\nEUR{nbu.nbu_eur}'
     telegram_bot_sendtext(bot_message=message)
     return render(request, 'currency/mirror.html', base_dict)
 
 def tested(request):
-    # TODO нужно добавить кнопку
     return render(request, 'currency/tested.html')
+
+def get_currency_to_base(request):
+    base_dict = dic.copy()
+    c.objects.create(
+        nbu_usd=dic['nbu_price_usd'],
+        nbu_eur = dic['nbu_price_eur'],
+        privat_usd_sale = dic[ 'privat_price_usd_sale'],
+        privat_usd_bay =  dic['privat_price_usd_bay'],
+        privat_eur_sale =  dic['privat_price_eur_sale'],
+        privat_eur_bay =  dic['privat_price_eur_bay']
+    )
+    return render(request, 'currency/button_to_base.html', base_dict)
