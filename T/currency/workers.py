84cb8180a8e255ff6cefc6669e4ae721b6c8d7e3
AleksanderYa <250000ye@gmail.com>
2021-08-11 12:02:17 +0300

    MonoWokers, PrivatWorker, NbuWorker

--- T/currency/workers.py
+++ T/currency/workers.py
@@ -4,20 +4,36 @@ import requests
 
 
 class ChangeDate:
-    def correct_data(self):
-        date = datetime.now()
-        month = str(date.month)
-        if len(month) < 2:
-            month = '0' + month
-            res = f'{date.day}.{month}.{date.year}'
-            return res
+    def __init__(self):
+        self.date = datetime.now()
+
+
+    def correct_month(self):
+        date_month= str(self.date.month)
+        if len(date_month) < 2:
+            self.month = '0' + date_month
+            return self.month
         else:
-            return f'{date.day}.{date.month}.{date.year}'
-#
+            return date_month
 
+    def date_to_privat(self):
+        res = f'{self.date.day}.{self.correct_month()}.{self.date.year}'
+        return res
 
+    def date_to_nbu(self):
+        res = f'{self.date.year}{self.correct_month()}{self.date.day}'
+        return res
 
-class BaseBankWorker(metaclass=ABCMeta):
+class BaseBankWorker(metaclass=ABCMeta): # Not need to use metaclass
+    def privat_exchangeRate_list(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)['exchangeRate']
+            except TypeError:
+                return func(*args, **kwargs)
+        return inner
+
+    @privat_exchangeRate_list
     def get_currency_json(self):
         try:
             url = requests.get(self.SITE)
@@ -26,20 +42,38 @@ class BaseBankWorker(metaclass=ABCMeta):
         except Exception as e:
             print(e)
 
-    # @abstractmethod
-    # def get_currency_info(self):
-    #     raise NotImplementedError("check_ripeness method not implemented!")
-
 class PrivatWorker(BaseBankWorker):
     date = ChangeDate()
-    SITE = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={date.correct_data()}'
+    date = date.date_to_privat()
+    SITE = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={date}'
 
 
 class NbuWorkers(BaseBankWorker):
-    pass
+    date = ChangeDate()
+    date = date.date_to_nbu()
+    SITE = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=' \
+           f'{date}&json'
+
 
 class MonoWorker(BaseBankWorker):
-    pass
+    SITE = 'https://api.monobank.ua/bank/currency'
 
-a = PrivatWorker()
-print(a.get_currency_json())
+
+
+
+if __name__ == '__main__':
+    a = MonoWorker()
+    aa = a.get_currency_json()
+    print(type(aa))
+    print(aa)
+
+    b = PrivatWorker()
+    bb = b.get_currency_json()
+    print(type(bb))
+    print(bb)
+
+    c = NbuWorkers()
+    cc = c.get_currency_json()
+    print(type(cc))
+    print(cc)
+
