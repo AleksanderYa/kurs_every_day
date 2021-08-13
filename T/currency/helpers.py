02f62255044f779988a19c035750573c3f77dc3c
Alex <sabakasrala@gmail.com>
2021-08-13 21:34:38 +0300

    optimizate create_currency_obj

--- T/currency/helpers.py
+++ T/currency/helpers.py
@@ -5,9 +5,21 @@ class BaseHelper(ABC):
     def __init__(self):
         self.create_obj = CreateObj()
 
-    @abstractmethod
-    def create_currency_obj(self, obj):
+    # @abstractmethod
+    # def create_currency_obj(self, obj):
+    #     raise NotImplementedError
+    def if_el(self, obj):
         raise NotImplementedError
+
+    def create_currency_obj(self, obj):
+        try:
+            for i in obj:
+                try:
+                    self.if_el(i)
+                except:
+                    pass
+        finally:
+            return self.create_obj
 
 class CreateObj:
 #####
@@ -19,54 +31,34 @@ class CreateObj:
 
 class PrivatHelper(BaseHelper):
 #####
-    def create_currency_obj(self, obj):
-        try:
-            for i in obj:
-                try:
-                    if i['currency'] == 'USD':
-                        self.create_obj.usd_buy = i['saleRate']
-                        self.create_obj.usd_sale = i['purchaseRate']
-                    elif i['currency'] == 'EUR':
-                        self.create_obj.euro_buy = i['saleRate']
-                        self.create_obj.euro_sale = i['purchaseRate']
-                except:
-                    pass
-        finally:
-            return self.create_obj
+    def if_el(self, i):
+        if i['currency'] == 'USD':
+            self.create_obj.usd_buy = i['saleRate']
+            self.create_obj.usd_sale = i['purchaseRate']
+        elif i['currency'] == 'EUR':
+            self.create_obj.euro_buy = i['saleRate']
+            self.create_obj.euro_sale = i['purchaseRate']
+
 
 class MonoHelper(BaseHelper):
 #####
-    def create_currency_obj(self, obj):
-        try:
-            for i in obj:
-                # try:
-                if i['currencyCodeA'] == 840 and i['currencyCodeB'] == 980:
-                    self.create_obj.usd_buy = i['rateSell']
-                    self.create_obj.usd_sale = i['rateBuy']
-                elif i['currencyCodeA'] == 978 and i['currencyCodeB'] == 980:
-                    self.create_obj.euro_buy = i['rateSell']
-                    self.create_obj.euro_sale = i['rateBuy']
-                # except:
-                #     pass
-        finally:
-            return self.create_obj
+    def if_el(self, i):
+        if i['currencyCodeA'] == 840 and i['currencyCodeB'] == 980:
+            self.create_obj.usd_buy = i['rateSell']
+            self.create_obj.usd_sale = i['rateBuy']
+        elif i['currencyCodeA'] == 978 and i['currencyCodeB'] == 980:
+            self.create_obj.euro_buy = i['rateSell']
+            self.create_obj.euro_sale = i['rateBuy']
 
 class NbuHelper(BaseHelper):
 #####
-    def create_currency_obj(self, obj):
-        try:
-            for i in obj:
-                try:
-                    if i['cc'] == 'USD':
-                        self.create_obj.usd_sale = i['rate']
-                        # self.create_obj.usd_sale = i['purchaseRate']
-                    elif i['cc'] == 'EUR':
-                        # self.create_obj.euro_buy = i['saleRate']
-                        self.create_obj.euro_sale = i['rate']
-                except:
-                    pass
-        finally:
-            return self.create_obj
+    def if_el(self, i):
+        if i['cc'] == 'USD':
+            self.create_obj.usd_sale = i['rate']
+            # self.create_obj.usd_sale = i['purchaseRate']
+        elif i['cc'] == 'EUR':
+            # self.create_obj.euro_buy = i['saleRate']
+            self.create_obj.euro_sale = i['rate']
 
 if __name__ == '__main__':
     a = PrivatHelper()
