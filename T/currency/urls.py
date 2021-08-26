ad243714fbd0448fc2fea6d7d6978479d9638d8a
AleksanderYa <250000ye@gmail.com>
2021-08-26 17:32:24 +0300

    work

--- T/currency/urls.py
+++ T/currency/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     re_path(r'^exchangetimesetup/(?P<time>\d+)/', ExchangeRateController.timer),
     path('bankrates/', ExchangeRateController.bunk_rates),
     path('exchangetimesetup/', ExchangeRateController.timer),
-    path('test/', ExchangeRateController.test)
+    path('test/', ExchangeRateController.test),
+    path('rates/', ExchangeRateController.get_all_banks)
 
 ]
