e7287df27d59ef4db82de7f1ae6d695a7ac96530
Alex <sabakasrala@gmail.com>
2021-08-26 22:54:08 +0300

    add views nbu and all banks

--- T/currency/urls.py
+++ T/currency/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, re_path
-from currency.views import ExchangeRateController
+from currency.views import ExchangeAllRate
+from currency.views import ExchangeNBURate
     
 
 urlpatterns = [
 
-    re_path(r'^exchangetimesetup/(?P<time>\d+)/', ExchangeRateController.timer),
-    path('bankrates/', ExchangeRateController.bunk_rates),
-    path('exchangetimesetup/', ExchangeRateController.timer),
-    path('test/', ExchangeRateController.test),
-    path('rates/', ExchangeRateController.get_all_banks)
+    # re_path(r'^exchangetimesetup/(?P<time>\d+)/', ExchangeRateController.timer),
+    # path('bankrates/', ExchangeRateController.bunk_rates),
+    # path('exchangetimesetup/', ExchangeRateController.timer),
+    # path('test/', ExchangeRateController.test),
+    path('rates/', ExchangeAllRate.as_view()),
+    path ('rates/nbu/', ExchangeNBURate.as_view())
 
 ]
--- T/currency/views.py
+++ T/currency/views.py
@@ -1,48 +1,30 @@
 from django.shortcuts import render, HttpResponse
-from currency.forms import UserForm
-from currency.models import Currency
+from django.views.generic.base import View
 
-class ExchangeRateController:
-    @staticmethod
-    def get_all_banks(request):
-        base = Currency.objects.all()
-        enum =  enumerate(base)
+from currency.models import Currency
+from currency.forms import UserForm
+
+class ExchangeRateController(View):
+    def __init__(self):
+        self.base = Currency.objects.all()
+
+class ExchangeAllRate(ExchangeRateController):
+    def get(self, request):
+        enum = enumerate(self.base)
         return render(
             request,
             'currency/table_all_banks.html',
             {'enum': enum}
         )
 
-    @staticmethod
-    def bunk_rates(request):
-        return render(request, 'currency/index.html')
-
-    @staticmethod
-    def test(request):
-        if request.method == "POST":
-            name = request.POST.get("name")
-            age = request.POST.get("age")     # получение значения поля age
-            return HttpResponse(f"<h2>Hello, {name}, - {age} </h2>")
-        else:
-            userform = UserForm()
-            return render(request, "currency/test.html", {"form": userform})
-
-    @staticmethod
-    def timer(request):
-        req = request.POST.get('timer_time') == 'Select work time'
-        if request.method == "POST":
-            time = request.POST.get('timer_time')
-            text = {
-                'text': f'Установлено...\nЧерез каждые {time} минут будет сделано дело!'
-            }
-            return render(request, "currency/timer.html", text)
-        elif req:
-            return render (request, "currency/timer.html", {'text': 'Выберите время из списка выше'})
-        else:
-            text = {
-                'text': 'Время работы бота не установленно'
-            }
-            return render(request, "currency/timer.html", text)
+class ExchangeNBURate(ExchangeRateController):
+    def get(self, request):
+        enum = enumerate (self.base)
+        return render (
+            request,
+            'currency/table_nbu_bank.html',
+            {'enum': enum}
+        )
 
 
 
@@ -56,4 +38,59 @@ class ExchangeRateController:
 
 
 
-
+    #
+    # @staticmethod
+    # def get_nbu_banks(request):
+    #     base = Currency.objects.all()
+    #     enum =  enumerate(base)
+    #     return render(
+    #         request,
+    #         'currency/table.html',
+    #         {'enum': enum}
+    #     )
+    #
+    #
+    # # @staticmethod
+    # # def bunk_rates(request):
+    # #     return render(request, 'currency/index.html')
+    # #
+    # # @staticmethod
+    # # def test(request):
+    # #     if request.method == "POST":
+    # #         name = request.POST.get("name")
+    # #         age = request.POST.get("age")     # получение значения поля age
+    # #         return HttpResponse(f"<h2>Hello, {name}, - {age} </h2>")
+    # #     else:
+    # #         userform = UserForm()
+    # #         return render(request, "currency/test.html", {"form": userform})
+    # #
+    # # @staticmethod
+    # # def timer(request):
+    # #     req = request.POST.get('timer_time') == 'Select work time'
+    # #     if request.method == "POST":
+    # #         time = request.POST.get('timer_time')
+    # #         text = {
+    # #             'text': f'Установлено...\nЧерез каждые {time} минут будет сделано дело!'
+    # #         }
+    # #         return render(request, "currency/timer.html", text)
+    # #     elif req:
+    # #         return render (request, "currency/timer.html", {'text': 'Выберите время из списка выше'})
+    # #     else:
+    # #         text = {
+    # #             'text': 'Время работы бота не установленно'
+    # #         }
+    # #         return render(request, "currency/timer.html", text)
+    # #
+    # #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
